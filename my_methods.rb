5ac0f51f8b63323c0e393d10a33291d8cd842b8f
Rukundo Eric <ericrukundo005@gmail.com>
2020-10-19 16:59:20 +0200

    Add my-all,my-any,my-none and my-count functions

--- my_methods.rb
+++ my_methods.rb
@@ -16,20 +16,39 @@ def my_select(array, filter)
   end
 end
 
-def my_all
-
+def my_all(array)
+  for i in 0...array.length
+    if(!yield array[i])
+      return false
+    end
+  end
+  return true
 end
 
-def my_any
-
+def my_any(array)
+  for i in 0...array.length
+    if(yield array[i])
+      return true
+    end
+  end
+  return false
 end
 
-def my_none
-
+def my_none(array)
+  for i in 0...array.length
+    if(yield array[i])
+      return false
+    end
+  end
+  return true
 end
 
-def my_count
-
+def my_count(array)
+  count = 0
+  for i in 1..array.length
+    count += 1
+  end
+  count
 end
 
 def my_map
@@ -52,3 +71,22 @@ end
 # my_select([3, 4, 5, 6, 2], 6) do |item|
 #   puts item
 # end
+#
+
+
+my_All_res = my_all(%w[dcd fdfdf dfdf dfdf dfdfd]) do |item|
+  true if item.length > 6
+end
+
+my_any_res = my_any(%w[dcd fdfdf dfdf dfdf dfdfdas]) do |item|
+  true if item.length > 6
+end
+
+my_none_res = my_none(%w[dcd fdfdf dfdf dfdf dfdf]) do |item|
+  true if item.length > 6
+end
+
+puts my_All_res
+puts my_any_res
+puts my_none_res
+puts my_count([43,34,35,'sds',3434]);
