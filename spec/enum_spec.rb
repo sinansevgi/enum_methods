839e918354bd735c2fd098d072ca7a281bc4ac14
daviidy <yaodavidarmel@gmail.com>
2020-11-03 10:21:21 +0000

    testing my_each_with_index and my_select

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -16,5 +16,19 @@ RSpec.describe Enumerable do
         expect(hash1.my_each { |n| n }).to eql(hash1.each { |i| i })
       end
     end
+
+    describe '#my_each_with_index' do
+      it 'should go through all elements and return the original array with values and index' do
+        expect(arr1.my_each_with_index { |n| n }).to eql(arr1.each { |i| i })
+        expect(range1.my_each_with_index { |n| n }).to eql(range1.each_with_index { |i| i })
+        expect(hash1.my_each_with_index { |n| n }).to eql(hash1.each_with_index { |i| i })
+      end
+    end
+
+    describe '#my_select' do
+      it 'return an array of even numbers' do
+        expect(arr1.my_select { |n| n.even? }).to eql(arr1.select { |i| i.even? })
+      end
+    end
   end
 end
